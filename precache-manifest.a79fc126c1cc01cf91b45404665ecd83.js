2a9cbb3f62fb397ab1208fbef2d755381d85522e
Arun Kumar Selvaraj <dev@ioak.org>
2019-10-29 20:44:09 +0530

    Updates

---
+++ precache-manifest.a79fc126c1cc01cf91b45404665ecd83.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5faefe96e619c60265709fdfc6468bb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba35249fd041cd5fd4b0",
+    "url": "/static/css/main.e3bd863a.chunk.css"
+  },
+  {
+    "revision": "478dd7159a8cc8f9ee49",
+    "url": "/static/js/2.0e1997af.chunk.js"
+  },
+  {
+    "revision": "ba35249fd041cd5fd4b0",
+    "url": "/static/js/main.88ce21f4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6fc52335e5235bddf0a979cd37ba982f",
+    "url": "/static/media/home_bg1.6fc52335.jpg"
+  },
+  {
+    "revision": "3b2c753ad8b4a05262f4cc0745dcbaf2",
+    "url": "/static/media/home_bg2.3b2c753a.jpg"
+  },
+  {
+    "revision": "a0d32f603f0c66b12ea4c5bc5280a7e3",
+    "url": "/static/media/home_bg3.a0d32f60.jpg"
+  },
+  {
+    "revision": "e6b35fe309506bcf111d76374ef96da2",
+    "url": "/static/media/home_bg4.e6b35fe3.jpg"
+  },
+  {
+    "revision": "5ef86055bf3433d08ee863e29465b23d",
+    "url": "/static/media/home_bg5.5ef86055.jpg"
+  },
+  {
+    "revision": "202eb7fa003e59bb8210da01d6e25a9a",
+    "url": "/static/media/home_bg6.202eb7fa.jpg"
+  },
+  {
+    "revision": "29d29873cf30bb2beee3a867d15e01b7",
+    "url": "/static/media/home_bg7.29d29873.jpg"
+  },
+  {
+    "revision": "1833fa8352577834bd42225dc2eb18a3",
+    "url": "/static/media/home_bg8.1833fa83.jpg"
+  },
+  {
+    "revision": "3b48b22e0b0d2386314b09f108cf05cf",
+    "url": "/static/media/login_bg.3b48b22e.jpg"
+  },
+  {
+    "revision": "f386b1d60b331fcba827a3c810b54a42",
+    "url": "/static/media/protekt_black.f386b1d6.svg"
+  },
+  {
+    "revision": "0157e4667b07c3bcb517f8a063df92dd",
+    "url": "/static/media/protekt_white.0157e466.svg"
+  }
+]);
